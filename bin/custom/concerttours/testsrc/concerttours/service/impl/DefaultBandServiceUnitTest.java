9669819e68b5b33213ebc35caa8b6d288abb01e1
DARYA <89320281+darishenko@users.noreply.github.com>
2023-11-20 12:08:55 +0300

    Task 2: Add a Unit Test for DefaultBandService

---
+++ bin/custom/concerttours/testsrc/concerttours/service/impl/DefaultBandServiceUnitTest.java
@@ -0,0 +1,71 @@
+package concerttours.service.impl;
+
+import concerttours.daos.BandDAO;
+import concerttours.model.BandModel;
+import de.hybris.bootstrap.annotations.UnitTest;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@UnitTest
+public class DefaultBandServiceUnitTest {
+    /**
+     * Name of test band.
+     */
+    private static final String BAND_CODE = "Ch00X";
+    /**
+     * Name of test band.
+     */
+    private static final String BAND_NAME = "Singers All";
+    /**
+     * History of test band.
+     */
+    private static final String BAND_HISTORY =
+            "Medieval choir formed in 2001, based in Munich famous for authentic monastic chants";
+    private DefaultBandService bandService;
+    private BandDAO bandDAO;
+    private BandModel bandModel;
+
+    @Before
+    public void setUp() {
+        bandService = new DefaultBandService();
+
+        bandDAO = mock(BandDAO.class);
+        bandService.setBandDAO(bandDAO);
+
+        bandModel = new BandModel();
+        bandModel.setCode(BAND_CODE);
+        bandModel.setName(BAND_NAME);
+        bandModel.setHistory(BAND_HISTORY);
+    }
+
+    /**
+     * This test tests and demonstrates that the Service's getAllBands method calls the DAOs' getBands method
+     * and returns the data it receives from it.
+     */
+    @Test
+    public void getAllBands_existingBand_returnBand() {
+        final List<BandModel> bandModels = Collections.singletonList(bandModel);
+        when(bandDAO.findBands()).thenReturn(bandModels);
+
+        final List<BandModel> result = bandService.getBands();
+
+        assertEquals("We should find one", 1, result.size());
+        assertEquals("And should equals what the mock returned", bandModel, result.get(0));
+    }
+
+    @Test
+    public void getBand_existingBandCode_returnBand() {
+        when(bandDAO.findBandsByCode(BAND_CODE)).thenReturn(Collections.singletonList(bandModel));
+
+        final BandModel result = bandService.getBandForCode(BAND_CODE);
+
+        assertEquals("Band should equals() what the mock returned", bandModel, result);
+    }
+}
