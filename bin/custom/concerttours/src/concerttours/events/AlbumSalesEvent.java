5a5fa21f24c71f9213c73aa0ef69055d37bb825b
DARYA <89320281+darishenko@users.noreply.github.com>
2023-11-28 20:59:55 +0300

    Task 3: Add a custom AlbumSalesEvent

---
+++ bin/custom/concerttours/src/concerttours/events/AlbumSalesEvent.java
@@ -0,0 +1,36 @@
+package concerttours.events;
+
+import de.hybris.platform.servicelayer.event.events.AbstractEvent;
+
+import java.awt.font.TextHitInfo;
+import java.util.List;
+
+public class AlbumSalesEvent extends AbstractEvent {
+    private final String name;
+    private final long albumSales;
+    private final List<String> songs;
+
+    public AlbumSalesEvent(final String name, final Long albumSales, final List<String> songs) {
+        super();
+        this.name = name;
+        this.albumSales = albumSales;
+        this.songs = songs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getAlbumSales() {
+        return albumSales;
+    }
+
+    public List<String> getSongs() {
+        return songs;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
---
+++ bin/custom/concerttours/src/concerttours/interceptors/AlbumSalesInterceptor.java
@@ -0,0 +1,61 @@
+package concerttours.interceptors;
+
+import concerttours.events.AlbumSalesEvent;
+import concerttours.model.AlbumModel;
+import de.hybris.platform.servicelayer.event.EventService;
+import de.hybris.platform.servicelayer.interceptor.InterceptorContext;
+import de.hybris.platform.servicelayer.interceptor.InterceptorException;
+import de.hybris.platform.servicelayer.interceptor.PrepareInterceptor;
+import de.hybris.platform.servicelayer.interceptor.ValidateInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Objects;
+
+public class AlbumSalesInterceptor implements ValidateInterceptor, PrepareInterceptor {
+    private static final long BIG_SALES = 50000L;
+    private static final long NEGATIVE_SALES = 0L;
+    private static final String ERROR_POSITIVE_ALBUM_SALES = "Album sales must be positive";
+
+    @Autowired
+    private EventService eventService;
+
+    @Override
+    public void onValidate(final Object model, final InterceptorContext ctx) throws InterceptorException {
+        if (model instanceof AlbumModel) {
+            final AlbumModel albumModel = (AlbumModel) model;
+            final Long sales = albumModel.getAlbumSales();
+
+            if (Objects.nonNull(sales) && sales.longValue() < NEGATIVE_SALES) {
+                throw new InterceptorException(ERROR_POSITIVE_ALBUM_SALES);
+            }
+        }
+    }
+
+    @Override
+    public void onPrepare(final Object model, final InterceptorContext ctx) throws InterceptorException {
+        if (model instanceof AlbumModel) {
+            final AlbumModel albumModel = (AlbumModel) model;
+
+            if (hasBecomeBig(albumModel, ctx)) {
+                eventService.publishEvent(new AlbumSalesEvent(albumModel.getName(), albumModel.getAlbumSales(),
+                        albumModel.getSongs()));
+            }
+        }
+    }
+
+    private boolean hasBecomeBig(final AlbumModel albumModel, final InterceptorContext ctx) {
+        final Long sales = albumModel.getAlbumSales();
+
+        if (Objects.nonNull(sales) && sales.longValue() >= BIG_SALES) {
+            if (ctx.isNew(albumModel)) {
+                return true;
+            } else {
+                final Long oldValue = getItemModelContext(albumModel).getOriginalValue(AlbumModel.ALBUMSALES);
+
+                return  Objects.isNull(oldValue) || oldValue.intValue() < BIG_SALES;
+            }
+        }
+
+        return false;
+    }
+}
